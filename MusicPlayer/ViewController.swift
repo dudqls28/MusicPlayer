110a088e2c5e26e1e522f0a01863d35202ff6d4d
yblim <dudqls28@naver.com>
2022-10-26 23:19:25 +0900

    (10.26) UI 기능 추가

--- MusicPlayer/ViewController.swift
+++ MusicPlayer/ViewController.swift
@@ -40,23 +40,72 @@ class ViewController: UIViewController, AVAudioPlayerDelegate{
         let timeText: String = String(format: "%02ld:%02ld:%02ld",minute ,second,milisecond )
         self.timeLabel.text = timeText
     }
-    func makAndFireTimer(){
-        //self.timer = Timer.
+    func makeAndFireTimer(){
+        self.timer = Timer.scheduledTimer(withTimeInterval:0.01, repeats: true,block: {[unowned self](timer:Timer) in
+            if self.progressSlider.isTracking {return}
+            
+            self.updateTimeLabelText(time: self.player.currentTime)
+            self.progressSlider.value = Float(self.player.currentTime)
+        })
+        self.timer.fire()
     }
-    
+    func invalidateTimer(){
+        self.timer.invalidate()
+        self.timer = nil
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.initializePlayer()
     }
 
 
-    @IBAction func touchUpPlayPauseButton(_ sender: Any) {
-        print("touch up")
+    @IBAction func touchUpPlayPauseButton(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        
+        if sender.isSelected{
+            self.player?.play()
+        }else{
+            self.player?.pause()
+        }
+        
+        if sender.isSelected{
+            self.makeAndFireTimer()
+        }else{
+            self.invalidateTimer()
+        }
     }
     
-    @IBAction func slideValueChanged(_ sender: Any) {
-        print("Change slider")
+    @IBAction func slideValueChanged(_ sender: UISlider) {
+        self.updateTimeLabelText(time: TimeInterval(sender.value))
+        if sender.isTracking {return}
+        self.player.currentTime = TimeInterval(sender.value)
     }
     
+    func audioPlayerDecodeErrorDidOccur(_ player: AVAudioPlayer, error: Error?) {
+        guard let error: Error = error else {
+            print("audio error")
+            return
+        }
+        
+        let message : String
+        message = "Audio Error \(error.localizedDescription)"
+        
+        let alert : UIAlertController = UIAlertController(title: "Info", message: message, preferredStyle: UIAlertController.Style.alert)
+        
+        let okAction : UIAlertAction = UIAlertAction(title: "confirm", style: UIAlertAction.Style.default){(action : UIAlertAction) -> Void in
+        self.dismiss(animated: true,completion: nil)
+        }
+        
+        alert.addAction(okAction)
+        self.present(alert,animated: true, completion: nil)
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        self.playPauseButton.isSelected = false
+        self.progressSlider.value = 0
+        self.updateTimeLabelText(time: 0)
+        self.invalidateTimer()
+    }
 }
 
